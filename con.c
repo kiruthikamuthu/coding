21aea1e76ebf8513053a50e10b4f279ad6e67ce3
kiruthikamuthu <kiruthika747@gmail.com>
2016-07-09 11:45:57 +0530

    Create con.c

---
+++ con.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+char ch;
+scanf("%c",&ch)
+if((ch=='a')||(ch=='A')||(ch=='e')||(ch=='E')||(ch=='i')||(ch=='I')||(ch=='o'||(ch=='O')||(ch=='u')||(ch=='U'))
+printf("char is vowel");
+else
+printf("char is consonant");
+return 0;
+}
