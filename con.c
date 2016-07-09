e9b1772be1c72df265df0a4cc96db46c42c7241a
kiruthikamuthu <kiruthika747@gmail.com>
2016-07-09 11:47:34 +0530

    Update con.c

--- con.c
+++ con.c
@@ -2,7 +2,7 @@
 int main()
 {
 char ch;
-scanf("%c",&ch)
+scanf("%c",&ch);
 if((ch=='a')||(ch=='A')||(ch=='e')||(ch=='E')||(ch=='i')||(ch=='I')||(ch=='o'||(ch=='O')||(ch=='u')||(ch=='U'))
 printf("char is vowel");
 else
