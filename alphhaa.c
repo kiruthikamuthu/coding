a3d07425daac74b1204e9e8cc4e56a378c1e7ed4
kiruthikamuthu <kiruthika747@gmail.com>
2016-07-09 15:24:22 +0530

    Update alphhaa.c

--- alphhaa.c
+++ alphhaa.c
@@ -4,7 +4,7 @@ int main()
 char ch;
 scanf("%d",&ch);
 if(ch=='a'||ch=='A'||ch=='e'||ch=='E'||ch=='i'||ch=='I'||ch=='o'||ch=='O'||ch=='u'||ch=='U')
-printf("%c is alphabet",ch);
+printf("%c is vowel",ch);
 else
 printf("%c is consonant",ch);
 return 0;
