c2730eb5dc63957571d2d190c67c44f06c98192c
kiruthikamuthu <kiruthika747@gmail.com>
2016-07-09 11:39:49 +0530

    Create vowelll.c

---
+++ vowelll.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+char ch;
+if((ch==a)||(ch==A)||(ch==e)||(ch==E)||(ch==i)||(ch==I)||(ch==o||(ch==O)||(ch==u)||(ch==U))
+printf("char is vowel");
+else
+printf("char is consonent");
+return 0;
+}
